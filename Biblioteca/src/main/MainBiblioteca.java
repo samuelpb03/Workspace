a6810899681c93922a1ad9136d64989de317b31a
XA19A5\DAM1 <samuelpb337@gmail.com>
2023-01-27 13:53:45 +0100

    Cambios

--- Biblioteca/src/main/MainBiblioteca.java
+++ Biblioteca/src/main/MainBiblioteca.java
@@ -1,10 +1,13 @@
 package main;
 
+import libros.Libro;
+
 public class MainBiblioteca {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Libro arrayLibros [] = new Libro[10];
 		
 		
 	}
